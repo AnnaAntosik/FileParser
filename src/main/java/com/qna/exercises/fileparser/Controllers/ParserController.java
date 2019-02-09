33132db01dd9d9df368113f31a5c2a95906fe05b
Anna Kuna <ana.kuna@gmail.com>
2019-02-09 18:16:05 +0100

    JSP view added

--- src/main/java/com/qna/exercises/fileparser/Controllers/ParserController.java
+++ src/main/java/com/qna/exercises/fileparser/Controllers/ParserController.java
@@ -3,6 +3,7 @@ package com.qna.exercises.fileparser.Controllers;
 import com.qna.exercises.fileparser.Services.ParserDispacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,13 +21,14 @@ public class ParserController {
     }
 
     @PostMapping("/parser")
-    public List<String[]> parseFile(@RequestParam("file") MultipartFile file) {
+    public String parseFile(@RequestParam("file") MultipartFile file, Model model) {
         List<String[]> data = null;
         try {
             data = parserDispacher.getFileFormat(file.getOriginalFilename(), file.getInputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return data;
+        model.addAttribute("data", data);
+        return "viewData";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/viewData.jsp
@@ -0,0 +1,19 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Table</title>
+</head>
+<body>
+<table>
+
+    <c:forEach items="${data}" var="row">
+        <tr>
+            <c:forEach items="${row}" var="cell">
+                <td>${cell}</td>
+            </c:forEach>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
